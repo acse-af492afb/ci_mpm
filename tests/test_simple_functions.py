b06c9cab616c9cffa855c2966a695f162638ec0f
Kelvin-233 <ay22@ic.ac.uk>
2022-10-18 11:47:44 +0100

    hi Nelsons

--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -22,5 +22,6 @@ class TestSimpleFunctions(object):
         ])
     def test_factorial(self, number, expected):
         '''Test our factorial function'''
+        a =123
         answer = factorial(number)
         assert answer == expected
